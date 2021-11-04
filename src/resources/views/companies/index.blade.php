dec1a69576cea5190c55e7a66baf5418c1d6d14d
Nisida Masaya <mamamanoma297@icloud.com>
2021-11-04 14:44:40 +0900

    Update:見た目の変更
    全体的な見た目の変更
    
    要素全体をリンクにした

--- src/resources/views/companies/index.blade.php
+++ src/resources/views/companies/index.blade.php
@@ -25,49 +25,51 @@
     <ul class="companies_list">
       @foreach ($companies as $company)
       <li class="companies_elements">
-        <p class="companies_name"><a class="companies_name" href="/companies/{{ $company->id }}">{{ $company->name }}</a></p>
-        <p class="deadline">締切日：
-          @if ($company->deadline)
-          {{ $company->deadline->format('Y年m月d日') }}
-          @else
-          null
+        <a class="companies_link" href="/companies/{{ $company->id }}">
+          <p class="companies_name">{{ $company->name }}</p>
+          <p class="deadline">締切日：
+            @if ($company->deadline)
+            {{ $company->deadline->format('Y年m月d日') }}
+            @else
+            null
+            @endif
+          </p>
+          <img src="{{ asset('img/no_image_square.jpg')}}" class="companies_pic">
+          <div class="entries_delete_btn">
+            @if(!($user->is_teacher()))
+            @if($entries[$company->id])
+            <form action="{{route('entries.destroy', $entries[$company->id]->id)}}" method="post">
+              {{ csrf_field() }}
+              {{ method_field('delete') }}
+              <button type="submit" class="btn btn-danger">取り消し</button>
+            </form>
+            @else
+            <form action="{{route('entries.store')}}" method='post'>
+              {{ csrf_field() }}
+              {{ method_field('POST') }}
+              <input type="submit" name="entry" value="エントリー" class="btn btn-success">
+              <input type="hidden" name="company_id" value="{{ $company->id }}">
+            </form>
+            @endif
+          </div>
           @endif
-        </p>
-        <img src="https://1.bp.blogspot.com/-D2I7Z7-HLGU/Xlyf7OYUi8I/AAAAAAABXq4/jZ0035aDGiE5dP3WiYhlSqhhMgGy8p7zACNcBGAsYHQ/s400/no_image_square.jpg" class="companies_pic">
-        <div class="entries_delete_btn">
-          @if(!($user->is_teacher()))
-          @if($entries[$company->id])
-          <form action="{{route('entries.destroy', $entries[$company->id]->id)}}" method="post">
-            {{ csrf_field() }}
-            {{ method_field('delete') }}
-            <button type="submit" class="btn btn-danger">取り消し</button>
-          </form>
-          @else
-          <form action="{{route('entries.store')}}" method='post'>
-            {{ csrf_field() }}
-            {{ method_field('POST') }}
-            <input type="submit" name="entry" value="エントリー" class="btn btn-success">
-            <input type="hidden" name="company_id" value="{{ $company->id }}">
-          </form>
-          @endif
-        </div>
-        @endif
-        @if($user->is_teacher())
-        @if($company->create_user_id == $user->id)
-        <div class="edit_delete_btn">
-          <a class="btn btn-success" href="/companies/{{ $company->id }}/edit" role="button">編集</a>
+          @if($user->is_teacher())
+          @if($company->create_user_id == $user->id)
+          <div class="edit_delete_btn">
+            <a class="btn btn-success" id="edit_btn" href="/companies/{{ $company->id }}/edit" role="button">編集</a>
 
-          <form action="{{route('companies.destroy', $company->id)}}" method='post' name="delete_form">
-            {{ csrf_field() }}
-            {{ method_field('DELETE') }}
-            <input type="submit" name="delete" class="btn btn-danger" value="削除">
-          </form>
-        </div>
-        @else
-        <div>&nbsp;</div>
-        <div>&nbsp;</div>
-        @endif
-        @endif
+            <form action="{{route('companies.destroy', $company->id)}}" method='post' name="delete_form">
+              {{ csrf_field() }}
+              {{ method_field('DELETE') }}
+              <input type="submit" name="delete" class="btn btn-danger" id="delete_btn" value="削除">
+            </form>
+          </div>
+          @else
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          @endif
+          @endif
+        </a>
       </li>
       @endforeach
     </ul>
